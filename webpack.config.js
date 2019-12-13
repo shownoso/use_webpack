a5bf78e7ab1eca4c3b52f5b946d87a979ce0b1cc
shownoso <457802359@qq.com>
2019-12-13 17:02:48 +0800

    update tree-shaking doc

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-  // mode: 'production',
-  mode: 'development',
-  optimization: {
-    usedExports: true,
-  },
+  mode: 'production',
+  // mode: 'development',
+  // optimization: {
+  //   usedExports: true,
+  // },
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'cheap-module-eval-source-map',
+  // devtool: 'cheap-module-eval-source-map',
   devServer: {
     port: 3300,
     open: true,
